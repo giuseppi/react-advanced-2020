52fb3327fb86824a66ba6e452abad038c283c3cb
Giuseppi Pelayo <32854561+giuseppi@users.noreply.github.com>
2023-01-17 14:17:52 -0800

    finished useState basics w arrays and objects

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import { data } from '../../../data';
+import React, { useState } from 'react'; // If you do not import { useState }, you must call it
+import { data } from '../../../data';    // by using React.useState()
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [ people, setPeople ] = useState(data);
+
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id); // filters the array and only returns
+    setPeople(newPeople);                                        // ids NOT matching the id passed in
+  }
+
+  return (
+    <React.Fragment>
+      {
+        people.map((person) => {
+          const { id, name } = person;
+          return <div key={ id } className='item'>
+            <h4>{ name }</h4>
+            <button className='btn' onClick={() => removeItem(id)}>remove</button>
+            </div>
+        })
+      }
+      <button className='btn' onClick={() => setPeople([])}>clear items</button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateArray;
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,37 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  /*
+  const [ person, setPerson ] = useState({
+    name: 'peter',
+    age: 24,
+    message: 'hello there!'
+  });
+
+const changeMessage = () => {
+    setPerson({...person, message : 'hello world'}); // utilize spread operator (...) to keep other values
+  };
+*/
+
+  // We can choose to create objects (like above), or create multiple useStates like below
+  const [ name, setName ] = useState('peter');
+  const [ age, setAge ] = useState('24');
+  const [ message, setMessage ] = useState('random message');
+
+  const changeMessage = () => {
+    setMessage('hello world');
+  };
+
+  return (
+    <React.Fragment>
+      <h3>{ name }</h3>
+      <h3>{ age }</h3>
+      <h3>{ message }</h3>
+      <button className='btn' onClick={() => changeMessage()}>
+        change message
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateObject;

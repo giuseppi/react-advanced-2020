61091e769399a61e649e5ae4f9d595bf2e6da49d
Giuseppi Pelayo <giuseppipelayo@gmail.com>
2023-01-19 12:50:50 -0800

    started useEffect

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
+
+const o = { name: "bob", item: 23 };
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  useEffect(() => {
+    console.log("call useEffect");
+  });
+  console.log("render component");
+
+  return (
+    <>
+      <h1>{ value }</h1>
+      <button>button</button>
+    </>
+  );
 };
 
 export default UseEffectBasics;

2117068e5fd31d0a7978721cff5ef810e6c58754
Giuseppi Pelayo <32854561+giuseppi@users.noreply.github.com>
2023-01-19 21:35:57 -0800

    working on useeffect cleanup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics';
-import Final from './tutorial/2-useEffect/final/1-useEffect-basics';
+import React from "react";
+import Setup from "./tutorial/2-useEffect/setup/2-useEffect-cleanup";
+import Final from "./tutorial/2-useEffect/final/2-useEffect-cleanup";
 function App() {
   return (
-    <div className='container'>
-      <Setup/>
+    <div className="container">
+      <Setup />
       {/* <Final/> */}
     </div>
-  )
+  );
 }
 
 export default App;
